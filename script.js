16af0574633a9c4836239b330f81c8f459f4877d
DoctorWhy2021 <vladyslav.sapeha@nure.ua>
2023-02-05 01:53:44 +0200

    DOM Manipulations Lesson Finished

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ title.style.color = 'red';
 
 //Work with onclick
 
-const redSqr = document.getElementById('red');
-const yellowSqr = document.getElementById('yellow');
-const greenSqr = document.getElementById('green');
+//const redSqr = document.getElementById('red');
+//const yellowSqr = document.getElementById('yellow');
+//const greenSqr = document.getElementById('green');
 
-redSqr.onclick =  () => console.log('red');
-yellowSqr.onclick = () => console.log('yellow');
-greenSqr.onclick = () => console.log('green');
+//redSqr.onclick =  () => console.log('red');
+//yellowSqr.onclick = () => console.log('yellow');
+//greenSqr.onclick = () => console.log('green');
+
+const squares = document.querySelectorAll('.colorSquare');
+let timesClicked = { 'red' : 0, 'yellow' : 0, 'green' : 0}
+squares.forEach(square => {
+    square.onclick = () => {
+        timesClicked[square.value]++;
+        square.innerText = timesClicked[square.value];
+    };
+});
+
+const  clearGameBtn = document.getElementById('clrGame');
+clearGameBtn.onclick = () => clearScores();
+
+
+function clearScores(){
+    squares.forEach(square => {
+        timesClicked[square.value] = 0;
+        square.innerText = '';});
+}
