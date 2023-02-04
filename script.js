0b7ee96a537212abdc8f5bb9d68650e569c8d130
DoctorWhy2021 <vladyslav.sapeha@nure.ua>
2023-02-05 00:52:07 +0200

    Sync with laptop

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let title = document.getElementById('title');
-title.innerText = 'Goodbye'
+title.innerText = 'Goodbye';
+title.style.color = 'red';
+
+//Work with onclick
+
+const redSqr = document.getElementById('red');
+const yellowSqr = document.getElementById('yellow');
+const greenSqr = document.getElementById('green');
+
+redSqr.onclick =  () => console.log('red');
+yellowSqr.onclick = () => console.log('yellow');
+greenSqr.onclick = () => console.log('green');
