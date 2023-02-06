240bcf6e18c50fe801a693d13ceded6b03cc209f
DoctorWhy2021 <78569620+DoctorWhy2021@users.noreply.github.com>
2023-02-06 20:04:52 +0200

    Working with JS

--- Exercises/script.js
+++ Exercises/script.js
@@ -1,3 +1,27 @@
 const billTotal = document.getElementById('billTotal');
+const tipPers = document.getElementById('tipPers');
+let pplAmount = document.getElementById('pplAmount');
 const totalAmount = document.getElementById('totalAmount');
 
+let pplNumb = Number(pplAmount.innerText);
+let bill = Number(billTotal.value);
+let tip = Number(tipPers.value);
+
+console.log(billTotal.innerText)
+function addPpl(){
+    pplNumb++;
+    pplAmount.innerText = pplNumb.toString();
+}
+
+function removePpl(){
+    if (pplNumb > 1){
+        pplNumb--;
+        pplAmount.innerText = pplNumb.toString();
+    }
+}
+//Calculation for total per person
+const totalPerPers = () => {
+
+    totalAmount.innerText = (bill * (tip/100) / pplNumb).toString();
+    console.log(bill)
+}
